b6d8fbdba4cd5642c10f97d999ced8cf42309b5c
Fatuma Ismail <44118528+timaismail90@users.noreply.github.com>
2020-12-10 09:48:40 -0600

    added content in the file

--- reverse.js
+++ reverse.js
@@ -1,3 +1,16 @@
 const args = process.argv
 
 
+// adding new content to see if it would be reflected in git
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
+  
+  console.log(getRandomInt(3));
+  // expected output: 0, 1 or 2
+  
+  console.log(getRandomInt(1));
+  // expected output: 0
+  
+  console.log(Math.random());
+  // expected output: a number from 0 to <1
