3e4ab9c941e6fdcd3eb71d08bea66d1b705fb87a
Fatuma Ismail <44118528+timaismail90@users.noreply.github.com>
2021-01-06 08:02:52 -0600

    fixed the eqArray function such that it compares two arrays and returns true if they are equal

--- eqArrays.js
+++ eqArrays.js
@@ -10,19 +10,38 @@ const assertEqual = function(actual, expected) {
 /* //Implement a function eqArrays which takes in two arrays 
 and returns true or false, based on a perfect match. */
 
-const eqArrays = function(arr1, arrTwo) {
-    for (let i = 0; i<= arrOne.length; i++) {
-        
+const eqArrays = function(arrOne, arrTwo) {
+    let status;
+    if(arrOne.length === arrTwo.length)
+    {
+        for(let i = 0; i < arrOne.length; i++)
+        {
+            if(arrOne[i] !== arrTwo[i])
+            {
+                status = false;
+                // console.log(arrOne[i], arrTwo[i]);
+            }
+            else {
+                console.log(arrOne[i], arrTwo[i])
+                status = true
+                
+            }
+        }
     }
+
+    else{status = false}
+   return status;
+}
+
     
 //  (let j=0; j<= arrTwo.length; j++)
 
     
-}
+
 eqArrays([1, 2, 3], [1, 2, 3]) // => true
 eqArrays([1, 2, 3], [3, 2, 1]) // => false
 
 eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
 eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays([1, 4, 3], [1, 2, 3]), true); // => should PASS
