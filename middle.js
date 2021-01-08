dfa6aee1450eeeba61108bfebcf50dad41a7035f
Fatuma Ismail <44118528+timaismail90@users.noreply.github.com>
2021-01-08 09:55:06 -0600

    made corrections on the middle function

--- middle.js
+++ middle.js
@@ -55,21 +55,21 @@ function assertArraysEqual (arrOne, arrTwo) {
 const middle = function(array) {
     array.sort(function(a,b) {return a -b});
     let newArr = [];
-    for (let i = 0; i < array.length; i++) {
+    let middle = Math.floor(array.length/2);
         if(array.length <= 2) {
             return newArr;
         } else {
             if(array.length % 2 ===1) {
-                newArr = array[Math.floor(array.length/2)];
-                console.log(newArr);
+                newArr = array[middle];
+                //console.log(newArr);
                 return newArr;
             } else {
-                newArr = array.splice([Math.floor(array.lenght/2) -1])
-                console.log(newArr);
+                newArr = array.push(array[middle-1], array[middle]);
+                //console.log(newArr);
                 return newArr;
             }
         }
-    }
+    
 
 }
 
@@ -78,15 +78,11 @@ const middle = function(array) {
 
 
 
-
-
-
-
-middle([1]) // => []
-middle([1, 2]) // => []
-middle([1, 2, 3]) // => [2]
-middle([1, 2, 3, 4, 5]) // => [3]
-middle([1, 2, 3, 4]) // => [2, 3]
+console.log(middle([1])) // => []
+console.log(middle([1, 2])) // => []
+console.log(middle([1, 2, 3])) // => [2]
+console.log(middle([1, 2, 3, 4, 5])) // => [3]
+console.log(middle([1, 2, 3, 4])) // => [2, 3]
 const arr = [5, 6, 7, 8, 9];
-middle(arr, [1, 2, 3, 4, 5, 6]) // => [3, 4]
-assertArraysEqual(arr, [5, 6, 7, 8, 9]);
+//middle(arr, [1, 2, 3, 4, 5, 6]) // => [3, 4]
+//assertArraysEqual(arr, [5, 6, 7, 8, 9]);
