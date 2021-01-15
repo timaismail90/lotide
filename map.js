d602a660fcabcb854b3bd4e410efceb508ce472a
Fatuma Ismail <44118528+timaismail90@users.noreply.github.com>
2021-01-14 20:45:59 -0600

    completed the map function which I created from scratch to understand how the inbuilt map works

--- map.js
+++ map.js
@@ -1,7 +1,16 @@
 const words = ["ground", "control", "to", "major", "tom"];
 
-const map = function() {
-    // empty for now :)
+const map = function(array, callback) {
+    const results = [];
+
+    for(let item of array) {
+        results.push(callback(item));
+    }
+    // console.log('array', array);
+    // console.log('callback', callback);
+   
+    return results;
+   
   }
 
 const results1 = map(words, word => word[0]);
