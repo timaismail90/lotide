b4e05355a1749e70c50e88ecc88b8a4180db66a0
Fatuma Ismail <44118528+timaismail90@users.noreply.github.com>
2021-01-14 20:14:47 -0600

    started implemeting map function

---
+++ map.js
@@ -0,0 +1,8 @@
+const words = ["ground", "control", "to", "major", "tom"];
+
+const map = function() {
+    // empty for now :)
+  }
+
+const results1 = map(words, word => word[0]);
+console.log(results1);
