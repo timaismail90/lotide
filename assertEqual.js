3c87dca6f0d1d096571555ad2271a2a47a0f3cea
Fatuma Ismail <44118528+timaismail90@users.noreply.github.com>
2021-01-19 09:15:23 -0600

    removed the test code and created new folder which has a file that contains the test code. Required the assertEqual function within that test file

--- assertEqual.js
+++ assertEqual.js
@@ -8,11 +8,8 @@ const assertEqual = function(actual, expected) {
 
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("School", "University");
-assertEqual(5, 5);
+
+
 
 
 module.exports = assertEqual;
