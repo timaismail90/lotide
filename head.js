b19f509788acabafc57f49a00cae41f3a1622444
Fatuma Ismail <44118528+timaismail90@users.noreply.github.com>
2021-01-19 10:25:30 -0600

    refractored head.js

--- head.js
+++ head.js
@@ -1,10 +1,8 @@
-const assertEqual = require('./assertEqual');
 const head = function(arr) {
-    for(let i = 0; i <= arr.length; i++){
+    for(let i = 0; i < arr.length; i++){
         return arr[0];
     }
 };
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head(["school", "Lighthouse", "Labs"]), "School");
+
+module.export = head();
