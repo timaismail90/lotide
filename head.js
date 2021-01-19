5f43b96f6423018a1b784def5385138e0b39b306
Fatuma Ismail <44118528+timaismail90@users.noreply.github.com>
2021-01-19 09:10:10 -0600

    removed the assertEqual function and instead, required it

--- head.js
+++ head.js
@@ -1,12 +1,4 @@
-const assertEqual = function(actual, expected) {
-    if(actual === expected) {
-        console.log(`Assertion Passed: ${actual} === ${expected}`)
-    } else {
-        console.log(`Assertion Failed: ${actual} !== ${expected}`)
-    };
-
-};
-
+const assertEqual = require('./assertEqual');
 const head = function(arr) {
     for(let i = 0; i <= arr.length; i++){
         return arr[0];
